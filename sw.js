b262d54a5dd2381309e2733f1ef66ee1bf181d28
zakkerooni <zakkerooni0804@gmail.com>
2026-04-07 04:17:03 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'meishi-v3';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'meishi-v4';
+const ASSETS = ['./', './index.html', '/manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
